d2956bd59ceeaf44b915da06934b6169f3c690b9
tahhaaa <139639493+tahhaaa@users.noreply.github.com>
2026-03-20 19:58:21 +0000

    Fix checkout city select binding

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,14 +1,17 @@
-import { SelectHTMLAttributes } from "react";
+import { forwardRef, SelectHTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
 
-export function Select(props: SelectHTMLAttributes<HTMLSelectElement>) {
-  return (
+export const Select = forwardRef<HTMLSelectElement, SelectHTMLAttributes<HTMLSelectElement>>(
+  ({ className, ...props }, ref) => (
     <select
+      ref={ref}
       {...props}
       className={cn(
         "h-12 w-full rounded-2xl border border-black/10 bg-white px-4 text-sm outline-none transition focus:border-beige",
-        props.className,
+        className,
       )}
     />
-  );
-}
+  ),
+);
+
+Select.displayName = "Select";
